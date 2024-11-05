4ff8c032103cdb9327c7d3b692e806827e5e8de1
Zvonko Bregar <zvonko.bregar@eimv.si>
2024-11-05 07:11:40 +0100

    removing SEVERANCE

--- SEVERANCE/tests/test_grep.py
+++
@@ -1,7 +0,0 @@
-from src.grep import grep
-import pytest
-
-
-def test_grep():
-    assert grep('^Author') == 'mbox.txt had 1798 lines that matched ^Author'
-    assert grep('^X-') == 'mbox.txt had 14368 lines that matched ^X-'
